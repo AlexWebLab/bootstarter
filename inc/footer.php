f2653bb18ffc511edcc55eba106e9d4d93ff9c68
Alessandro <info@alexweblab.com>
2017-01-13 14:32:45 +0800

    added .visible-xxs

--- inc/footer.php
+++ inc/footer.php
@@ -6,8 +6,8 @@
     	</footer><!-- #colophon -->
     </div><!-- #page -->
 
-    <div class="device-xs visible-xs"></div><div class="device-sm visible-sm"></div><div class="device-md visible-md"></div><div class="device-lg visible-lg"></div>
-    <div id="display_classes"><div class="device-xs visible-xs">XS</div><div class="device-sm visible-sm">SM</div><div class="device-md visible-md">MD</div><div class="device-lg visible-lg">LG</div><div id="window_width"></div></div>
+    <div class="device-xxs visible-xxs"></div><div class="device-xs visible-xs"></div><div class="device-sm visible-sm"></div><div class="device-md visible-md"></div><div class="device-lg visible-lg"></div>
+    <div id="display_classes"><div class="device-xxs visible-xxs">XXS</div><div class="device-xs visible-xs">XS</div><div class="device-sm visible-sm">SM</div><div class="device-md visible-md">MD</div><div class="device-lg visible-lg">LG</div><div id="window_width"></div></div>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery.mobile.custom.min.js"></script>
